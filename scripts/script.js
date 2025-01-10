f4ad0ce6ba94e7f690982b010383c491d72ce252
Farhan Tanvir <farhantanvir200000@gmail.com>
2025-01-10 21:16:35 +0600

    File initialized

---
+++ scripts/script.js
@@ -0,0 +1,30 @@
+function toggleMenu() {
+  let x = document.querySelector(".child");
+ x.style.display =  (x.style.display === "none") ? "block" : "none"
+}
+
+function clicked() {
+  let y = document.querySelector(".visibled");
+  let z = document.querySelector(".arrow");
+  y.style.display = (y.style.display === "none") ? "block" : "none";
+  z.style.transform = !(y.style.display === "none") ? "rotate(-180deg)" : "rotate(-0deg)";
+}
+
+function clicked1() {
+  let y = document.querySelector(".visibled1");
+  let z = document.querySelector(".arrow1");
+  y.style.display = (y.style.display === "none") ? "block" : "none";
+  z.style.transform = !(y.style.display === "none") ? "rotate(-180deg)" : "rotate(0deg)";
+}
+function clicked2() {
+  let y = document.querySelector(".visibled2");
+  let z = document.querySelector(".arrow2");
+  y.style.display = (y.style.display === "none") ? "block" : "none";
+  z.style.transform = !(y.style.display === "none") ? "rotate(-180deg)" : "rotate(0deg)";
+}
+function clicked3() {
+  let y = document.querySelector(".visibled3");
+  let z = document.querySelector(".arrow3");
+  y.style.display = (y.style.display === "none") ? "block" : "none";
+  z.style.transform = !(y.style.display === "none") ? "rotate(-180deg)" : "rotate(0deg)";
+}
